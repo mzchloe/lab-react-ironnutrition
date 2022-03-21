0c44ac7b29af90288fb72ae02c971aaa90b082fc
Jeanet Lin <linett@gmail.com>
2022-03-21 23:06:19 +0100

    iteration 2 - done except text alignment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import './App.css';
 import foods from './foods.json';
 import React, { useState } from 'react';
+import FoodBox from './components/FoodBox/FoodBox';
 
 function App() {
   const [foodList, setFoodList] = useState(foods);
 
   return (
     <div className="App">
-      {foodList.map((food) => (
+
+<FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}} />
+      {/* {foodList.map((food) => (
         <div>
           <p> {food.name} </p>
           <img src={food.image} alt={food.image} width={100} />
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
---
+++ src/components/FoodBox/FoodBox.js
@@ -0,0 +1,30 @@
+import { Card, Col, Button } from 'antd';
+
+
+// Iteration 2
+export default function FoodBox(props) {
+
+    const totalCal = () => {
+        const totalCal =props.food.calories * props.food.servings
+    return totalCal
+    }
+
+  return (
+    <Col >
+      <Card
+        title={props.food.name}
+        style={{ width: 230, height: 300, margin: 10 }}
+      >
+        <img src={props.food.image} height={60} alt="food" />
+        <p>Calories: {props.food.calories}</p>
+        <p>Serving: <b> {props.food.servings}</b></p>
+        <p>
+          <b>Total Calories: {totalCal()}</b> kcal
+        </p>
+        <Button type="primary"> Delete </Button>
+      </Card>
+    </Col>
+  );
+}
+
+
