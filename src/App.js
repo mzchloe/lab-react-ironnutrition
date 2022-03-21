7dd05652454297f19ae9911dd22e0adbba294cc8
Jeanet Lin <linett@gmail.com>
2022-03-21 14:35:38 +0100

    iteration 1 - done, but nothing is rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import foods from './foods.json';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [foodList, setFoodList] = useState(foods);
+/* console.log(foodList) */
 
+  foodList.map((food) => {
+    /* console.log(food) */
+    return (
+      <div className="App">
+      <h1>Food List</h1>
+        <div>
+          <p> {food.name} </p>
+          <img src={food.image} alt={food.image} width={100} />
+        </div>
+      </div>
+    );
+  })
+}
 export default App;
